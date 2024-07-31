8b5303df2cabcdbabac8df89a3621fa382f3043b
krzszymala <krz.szymala@gmail.com>
2024-07-31 20:39:31 +0200

    Wersja 1.3 -Połączenie z Mongoose

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 const open = require('open');
+const mongoose = require('mongoose'); // Poprawiono błąd w imporcie Mongoose
+
+// Wczytanie zmiennych środowiskowych
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -15,9 +19,25 @@ app.use(cors({
 }));
 app.use(bodyParser.json());
 
+// Połączenie z MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB', error);
+});
+
 // Serwowanie statycznych plików z katalogu build
 const buildPath = path.resolve(__dirname, '../build');
 app.use(express.static(buildPath));
+
+// Endpointy dla API
+const participantRoutes = require('./routes/participants');
+app.use('/api/participants', participantRoutes);
 
 // Serwowanie pliku index.html dla wszystkich żądań, które nie pasują do żadnych innych endpointów
 app.get('*', (req, res) => {
