21f6d15f8da670a43ccdb61727f79fb0052694e8
krzszymala <krz.szymala@gmail.com>
2024-08-06 00:34:35 +0200

    Added translations in index.js

--- public/index.js
+++ public/index.js
@@ -17,7 +17,7 @@ i18n
     fallbackLng: 'en',
     debug: true,
     backend: {
-      loadPath: '/locales/{{lng}}/translation.json', // Upewnij się, że ta ścieżka jest poprawna
+        loadPath: '/public/locales/{{lng}}/translation.json', // Upewnij się, że ta ścieżka jest poprawna
     },
     interpolation: {
       escapeValue: false, // React already does escaping
