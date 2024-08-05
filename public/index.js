82048b11ade3a414ad1378b9781d30a376a123ba
krzszymala <krz.szymala@gmail.com>
2024-08-06 00:27:20 +0200

    Added translations in index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+// Inicjalizacja i18next
+i18n
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    backend: {
+      loadPath: '/locales/{{lng}}/translation.json', // Upewnij się, że ta ścieżka jest poprawna
+    },
+    interpolation: {
+      escapeValue: false, // React already does escaping
+    },
+  });
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
